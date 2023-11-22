63f3e3bb2ad64eac758e5e807f6e0ba143954e01
Ole-Christoffer Granmo <ole.granmo@uia.no>
2023-11-22 19:53:37 +0100

    Update

--- ReutersTextCategorizationTransformerDemoKerasV2.py
+++ ReutersTextCategorizationTransformerDemoKerasV2.py
@@ -71,9 +71,11 @@ def count_tokens(X_indices, X_indptr, word_profile_data, word_profile_indices, w
             target_word_refined_profile = target_word_refined_profile * other_word_profile
             target_word_refined_profile = target_word_refined_profile * other_word_profile_2
 
-            for k in range(target_word_refined_profile.shape[0]):
-                if target_word_refined_profile[k] < profile_threshold:
-                    target_word_refined_profile[k] = 0
+            sorted_indices = np.argsort(-1*target_word_refined_profile)
+            target_word_refined_profile[sorted_indices[5:]] = 0
+#            for k in range(target_word_refined_profile.shape[0]):
+#                if target_word_refined_profile[k] < profile_threshold:
+#                    target_word_refined_profile[k] = 0
 
             # The refiend profile is added to the document vector
             document_vector += target_word_refined_profile
