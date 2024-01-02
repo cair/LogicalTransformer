0f2a674ddcf5f3fa2bc806a39ce5cb8d33817ab0
Ole-Christoffer Granmo <ole.granmo@uia.no>
2024-01-02 09:54:13 +0100

    Update

--- LogicalTransformerIMDbDemo.py
+++ LogicalTransformerIMDbDemo.py
@@ -27,10 +27,49 @@ import keras
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import chi2
-from sklearn.metrics import recall_score, precision_score, accuracy_score
+from sklearn.metrics import recall_score, precision_score, accuracy_score, roc_auc_score
+import matplotlib.pyplot as plt
+from sklearn.metrics import PrecisionRecallDisplay
+import matplotlib.ticker as mticker
 
 examples = 5000
-positive_sample_p = 0.1
+context_size = 5
+profile_size = 50
+
+def plot_precision_recall_curve(scores, labels):
+    max_score = scores.max(axis=1)
+    max_score_index = scores.argmax(axis=1)
+    sorted_index = np.argsort(-1*max_score)
+
+    precision = []
+    recall = []
+    class_sum = []
+
+    correct = 0.0
+    total = 0.0
+    for i in sorted_index:
+        if max_score_index[i] == Y_test[i]:
+            correct += 1
+        total += 1
+
+        if total > 100:
+        	precision.append(correct/total)
+	        recall.append(total/sorted_index.shape[0])
+        	class_sum.append(max_score[i]) 
+
+        if total % 1 == 10:
+            print("%d %.2f %.2f" % (max_score[i], total/sorted_index.shape[0], correct/total))
+
+    plt.plot(class_sum, precision)
+    #plt.plot(recall, precision)
+
+    plt.grid()
+    plt.xlabel("Max Class Sum")
+    plt.ylabel("Accuracy")
+    plt.savefig('Figure.pdf')
+    plt.show()
+
+#positive_sample_p = 0.1
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -44,11 +83,11 @@ parser.add_argument("--epochs", default=40, type=int)
 parser.add_argument("--clause_drop_p", default=0.75, type=float)
 parser.add_argument("--max-ngram", default=1, type=int)
 parser.add_argument("--features", default=100, type=int)
-parser.add_argument("--imdb-num-words", default=5000, type=int)
+parser.add_argument("--imdb-num-words", default=10000, type=int)
 parser.add_argument("--imdb-index-from", default=2, type=int)
 args = parser.parse_args()
 
-_LOGGER.info("Preparing dataset")
+_LOGGER.info("Preparing dataset...")
 logging.basicConfig(level=logging.INFO)
 
 train, test = keras.datasets.imdb.load_data(num_words=args.imdb_num_words, index_from=args.imdb_index_from)
@@ -60,140 +99,176 @@ word_to_id = {k: (v + args.imdb_index_from) for k, v in word_to_id.items()}
 word_to_id["<PAD>"] = 0
 word_to_id["<START>"] = 1
 word_to_id["<UNK>"] = 2
-_LOGGER.info("Preparing dataset.... Done!")
-
-_LOGGER.info("Producing bit representation...")
+_LOGGER.info("Preparing dataset... Done!")
 
 id_to_word = {value: key for key, value in word_to_id.items()}
 
-training_documents = []
+tokens = {}
 for i in range(train_y.shape[0]):
-    terms = []
-    for word_id in train_x[i]:
-        terms.append(id_to_word[word_id].lower())
+	for token_id in train_x[i]:
+		tokens[id_to_word[token_id]] = True
 
-    training_documents.append(terms)
+tms = []
+for target_token in ['bad']:#tokens.keys():
+	_LOGGER.info("Producing self-attention datasets...")
 
-testing_documents = []
-for i in range(test_y.shape[0]):
-    terms = []
-    for word_id in test_x[i]:
-        terms.append(id_to_word[word_id].lower())
+	training_documents = []
+	training_class = []
+	testing_documents = []
+	testing_class = []
 
-    testing_documents.append(terms)
+	for i in range(train_y.shape[0]):
+		focus_token_position = 0
+		for focus_token_id in train_x[i]:
+			focus_token = id_to_word[focus_token_id]
+			if focus_token != target_token:
+				focus_token_position += 1
+				continue
 
-vectorizer_X = CountVectorizer(
-    tokenizer=lambda s: s,
-    token_pattern=None,
-    ngram_range=(1, args.max_ngram),
-    lowercase=False,
-    binary=True
-)
+			terms = []
+			context_token_position = 0
+			for context_token_id in train_x[i]:
+				if np.abs(context_token_position - focus_token_position) <= context_size and context_token_position != focus_token_position:
+					terms.append(str(context_token_position - focus_token_position) + ":" + id_to_word[context_token_id].lower())
 
-X_train = vectorizer_X.fit_transform(training_documents)
-feature_names = vectorizer_X.get_feature_names_out()
-Y_train = train_y.astype(np.uint32)
+					#if id_to_word[context_token_id].lower() != target_token:
+					#	terms.append(id_to_word[context_token_id].lower())
 
-X_test = vectorizer_X.transform(testing_documents)
-Y_test = test_y.astype(np.uint32)
-_LOGGER.info("Producing bit representation... Done!")
+				context_token_position += 1
 
-_LOGGER.info("Selecting Features....")
+			if train_y[i] == 1:
+				print(terms)
+			training_documents.append(terms)
+			training_class.append(train_y[i])
+			focus_token_position += 1
 
-SKB = SelectKBest(chi2, k=args.features)
-SKB.fit(X_train, Y_train)
+			if len(training_documents) >= examples:
+				break
+		
+		if len(training_documents) >= examples:
+			break
 
-selected_features = SKB.get_support(indices=True)
-X_train = SKB.transform(X_train).toarray().astype(np.uint32)
-X_test = SKB.transform(X_test).toarray().astype(np.uint32)
+	positive = 0
+	negative = 0
+	for i in range(test_y.shape[0]):
+		focus_token_position = 0
+		for focus_token_id in test_x[i]:
+			focus_token = id_to_word[focus_token_id]
+			if focus_token != target_token:
+				focus_token_position += 1
+				continue
 
-_LOGGER.info("Selecting Features.... Done!")
+			terms = []
+			context_token_position = 0
+			for context_token_id in test_x[i]:
+				if np.abs(context_token_position - focus_token_position) <= context_size and context_token_position != focus_token_position:
+					terms.append(str(context_token_position - focus_token_position) + ":" + id_to_word[context_token_id].lower())
 
-# Train one TM per token (feature) per class, producing a class specific token model
+					#if id_to_word[context_token_id].lower() != target_token:
+					#	terms.append(id_to_word[context_token_id].lower())
 
-tms = {}
-for i in range(2):
-	tms[i] = []
-	print("\nTraining token models for class", i, "\n")
-	for j in range(args.features):
-		# This is where you create a local perspective for each token (self-attention)
+				context_token_position += 1
 
-		tm = TMClassifier(10, 100, 1.1)
+			if test_y[i] == 1 and word_to_id[target_token] in test_x[i]:
+				positive += 1
+				print(terms)
 
-		# Extract prediction target from column 'j' in X_train, only looking at training examples from class 'i'
-		Y_train_token = X_train[:,j][Y_train==i].reshape(-1)
+			if test_y[i] == 0 and word_to_id[target_token] in test_x[i]:
+				negative += 1
+				print("***", terms)
 
-		# Remove prediction target from column 'j' in X_train
-		cols = np.arange(X_train.shape[1]) != j
-		X_train_token = X_train[:,cols][Y_train==i]
+			testing_documents.append(terms)
+			testing_class.append(test_y[i])
+			focus_token_position += 1
 
-		X_train_token_0 = X_train_token[Y_train_token==0]
-		X_train_token_1 = X_train_token[Y_train_token==1]
+			if len(testing_documents) >= examples:
+				break
+		
+		if len(testing_documents) > examples:
+			break
 
-		X_train_token_balanced = np.zeros((examples, X_train_token.shape[1]), dtype=np.uint32)
-		Y_train_token_balanced = np.zeros(examples, dtype=np.uint32)
-		for epoch in range(1):	
-			for k in range(examples):
-				if np.random.rand() <= positive_sample_p:
-					X_train_token_balanced[k,:] = X_train_token_1[np.random.randint(X_train_token_1.shape[0]),:]
-					Y_train_token_balanced[k] = 1
-				else:
-					X_train_token_balanced[k,:] = X_train_token_0[np.random.randint(X_train_token_0.shape[0]),:]
-					Y_train_token_balanced[k] = 0
+	if positive + negative > 0:
+		print(positive/(positive+negative), (positive + negative)/test_y.shape[0])
 
-			tm.fit(X_train_token_balanced, Y_train_token_balanced)
+	_LOGGER.info("Producing self-attention datasets... Done")
 
-		# Create test data for token prediction
-		Y_test_token = X_test[:,j][Y_test==i].reshape(-1)
-		cols = np.arange(X_test.shape[1]) != j
-		X_test_token = X_test[:,cols][Y_test==i]
+	_LOGGER.info("Producing bit representation...")
 
-		X_test_token_0 = X_test_token[Y_test_token==0]
-		X_test_token_1 = X_test_token[Y_test_token==1]
+	vectorizer_X = CountVectorizer(
+	    tokenizer=lambda s: s,
+	    token_pattern=None,
+	    ngram_range=(1, args.max_ngram),
+	    lowercase=False,
+	    binary=True
+	)
 
-		if (X_test_token_0.shape[0] == 0 or X_test_token_1.shape[0] == 0):
-			continue
+	X_train = vectorizer_X.fit_transform(training_documents).toarray().astype(np.uint32)
+	feature_names = vectorizer_X.get_feature_names_out()
+	Y_train = np.array(training_class).astype(np.uint32)
 
-		X_test_token_balanced = np.zeros((examples, X_test_token.shape[1]), dtype=np.uint32)
-		Y_test_token_balanced = np.zeros(examples, dtype=np.uint32)
-		for k in range(examples):
-			if (np.random.rand() <= positive_sample_p):
-				X_test_token_balanced[k,:] = X_test_token_1[np.random.randint(X_test_token_1.shape[0]),:]
-				Y_test_token_balanced[k] = 1
-			else:
-				X_test_token_balanced[k,:] = X_test_token_0[np.random.randint(X_test_token_0.shape[0]),:]
-				Y_test_token_balanced[k] = 0
+	print("******", X_train.shape)
 
-		Y_test_token_balanced_predicted = tm.predict(X_test_token_balanced)
+	X_test = vectorizer_X.transform(testing_documents).toarray().astype(np.uint32)
+	Y_test = np.array(testing_class).astype(np.uint32)
 
-		print("\tToken: %s Accuracy: %.2f%% Precision: %.2f Recall: %.2f" % (feature_names[selected_features[j]], 100*accuracy_score(Y_test_token_balanced, Y_test_token_balanced_predicted), 100*precision_score(Y_test_token_balanced, Y_test_token_balanced_predicted), 100*recall_score(Y_test_token_balanced, Y_test_token_balanced_predicted)))
+	_LOGGER.info("Producing bit representation... Done!")
 
-		# Store Tsetlin machine for token 'j' of class 'i'
-		tms[i].append(tm)
+	# This is where you create a local perspective for each token (self-attention)
 
-# Perform composite classification using the individual Tsetlin machines
+	tm = TMClassifier(1000, 10000, 1.0, weighted_clauses=True, max_included_literals=32)
 
-class_sums = np.zeros((2, Y_test.shape[0]))
+	tm.fit(X_train, Y_train)
+	tm.fit(X_train, Y_train)
 
-# Calculate composite clause sum per class
-for i in range(2):
-	# Add opp clause sum scores from each token Tsetlin machine
-	for j in range(args.features):
-		# Create input to token model
-		cols = np.arange(X_test.shape[1]) != j
-		X_test_token = X_test[:,cols]
+	(Y_test_predicted, Y_test_predicted_scores)  = tm.predict(X_test, return_class_sums=True)
 
-		print(X_test_token.shape)
+	sorted_indexes = np.argsort(-1*Y_test_predicted_scores[:,0])
 
-		Y_test_predicted_token, Y_test_scores_token = tms[i][j].predict(X_test_token, return_class_sums=True)
+	print(sorted_indexes.shape)
+	print("Score:", Y_test_predicted_scores[sorted_indexes[0:10], 0], Y_test[sorted_indexes[0:10]])
+	for k in range(X_test.shape[1]):
+		if X_test[sorted_indexes[0], k] == 1:
+			print(feature_names[k], end=' ')
+	print()
 
-		# Measure ability of token model to match the example. The class with the most fitting token model gets the highest score.
-		Y_test_scores_token_combined = np.where(X_test[:,j] == 1, Y_test_scores_token[:,1] - Y_test_scores_token[:,0], Y_test_scores_token[:,0] - Y_test_scores_token[:,1])
-		#Y_test_scores_token_combined = np.where(X_test[:,j] == 1, Y_test_scores_token[:,1] - Y_test_scores_token[:,0], 0)
+	plot_precision_recall_curve(Y_test_predicted_scores, Y_test)
 
-		class_sums[i] += Y_test_scores_token_combined
+	print("\tToken: %s AUC: %.2f%% AUC: %.2f%% Precision: %.2f Recall: %.2f Count: %d Precision: %.2f Recall: %.2f Count: %d" % (target_token, 100*roc_auc_score(Y_test, Y_test_predicted_scores[:,0]), 100*roc_auc_score(Y_test, Y_test_predicted_scores[:,1]), 100*precision_score(Y_test, Y_test_predicted), 100*recall_score(Y_test, Y_test_predicted), Y_test.sum(), 100*precision_score(1-Y_test, 1-Y_test_predicted), 100*recall_score(1-Y_test, 1-Y_test_predicted), (1-Y_test).sum()))
 
-# The class with the largest composite class sum wins
-Y_test_predicted = class_sums.argmax(axis=0)
+	print("\nPositive Polarity:", end=' ')
+	literal_importance = tm.literal_importance(1, negated_features=False, negative_polarity=False).astype(np.int32)
+	sorted_literals = np.argsort(-1*literal_importance)[0:profile_size]
+	for k in sorted_literals:
+		if literal_importance[k] == 0:
+			break
 
-print("\nClass prediction accuracy:", 100*(Y_test_predicted == Y_test).mean())
+		print(feature_names[k], end=' ')
+
+	literal_importance = tm.literal_importance(1, negated_features=True, negative_polarity=False).astype(np.int32)
+	sorted_literals = np.argsort(-1*literal_importance)[0:profile_size]
+	for k in sorted_literals:
+		if literal_importance[k] == 0:
+			break
+
+		print("¬" + feature_names[k - X_train.shape[1]], end=' ')
+
+	print()
+	print("\nNegative Polarity:", end=' ')
+	literal_importance = tm.literal_importance(1, negated_features=False, negative_polarity=True).astype(np.int32)
+	sorted_literals = np.argsort(-1*literal_importance)[0:profile_size]
+	for k in sorted_literals:
+		if literal_importance[k] == 0:
+			break
+
+		print(feature_names[k], end=' ')
+
+	literal_importance = tm.literal_importance(1, negated_features=True, negative_polarity=True).astype(np.int32)
+	sorted_literals = np.argsort(-1*literal_importance)[0:profile_size]
+	for k in sorted_literals:
+		if literal_importance[k] == 0:
+			break
+
+		print("¬" + feature_names[k - X_train.shape[1]], end=' ')
+	print()
+
+	tms.append(tm)
