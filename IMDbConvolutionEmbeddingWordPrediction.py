26510b52ceff5bca8383ff9138994820c9bdaf88
Ole-Christoffer Granmo <ole.granmo@uia.no>
2024-01-14 16:59:09 +0100

    Update

--- IMDbConvolutionEmbeddingWordPrediction.py
+++ IMDbConvolutionEmbeddingWordPrediction.py
@@ -137,18 +137,14 @@ for i in range(epochs):
 		stop_training = time()
 
 		start_testing = time()
-		Y_test_predicted = np.zeros(0, dtype=np.uint32)
-		for batch in range(batches):
-			Y_test_predicted = np.concatenate((Y_test_predicted, tm.predict(X_test[batch*batch_size_test:(batch+1)*batch_size_test])))
-		result_test = 100*(Y_test_predicted == Y_test[:batch_size_test*batches]).mean()
-		f1_test = f1_score(Y_test[:batch_size_test*batches], Y_test_predicted, average='macro')
+		Y_test_predicted = tm.predict(X_test[batch*batch_size_test:(batch+1)*batch_size_test])
+		result_test = 100*(Y_test_predicted == Y_test[batch*batch_size_test:(batch+1)*batch_size_test]).mean()
+		f1_test = f1_score(Y_test[batch*batch_size_test:(batch+1)*batch_size_test], Y_test_predicted, average='macro')
 		stop_testing = time()
 
-		Y_train_predicted = np.zeros(0, dtype=np.uint32)
-		for batch in range(batches):
-			Y_train_predicted = np.concatenate((Y_train_predicted, tm.predict(X_train[batch*batch_size_train:(batch+1)*batch_size_train])))
-		result_train = 100*(Y_train_predicted == Y_train[:batch_size_train*batches]).mean()
+		Y_train_predicted = tm.predict(X_train[batch*batch_size_train:(batch+1)*batch_size_train])
+		result_train = 100*(Y_train_predicted == Y_train[batch*batch_size_train:(batch+1)*batch_size_train]).mean()
 
-		f1_train = f1_score(Y_train[:batch_size_train*batches], Y_train_predicted, average='macro')
+		f1_train = f1_score(Y_train[batch*batch_size_train:(batch+1)*batch_size_train], Y_train_predicted, average='macro')
 
 		print("#%d/%d Accuracy Test: %.2f%% Accuracy Train: %.2f%% Training: %.2fs Testing: %.2fs" % (batch+1, i+1, result_test, result_train, stop_training-start_training, stop_testing-start_testing))
